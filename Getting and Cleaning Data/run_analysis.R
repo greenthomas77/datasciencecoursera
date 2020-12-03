12ef7a120a5fdaa828b54da96b7c4bc81d448c1c
tgreen <72272255+greenthomas77@users.noreply.github.com>
2020-12-03 09:00:29 -0500

    Add files via upload

---
+++ Getting and Cleaning Data/run_analysis.R
@@ -0,0 +1,32 @@
+library(dplyr)
+
+filename <- "Coursera_DS3_Final.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+# Simple named list: 
+list(mean = mean, median = median)
+
+# Auto named with `tibble::lst()`: 
+tibble::lst(mean, median)
+
+# Using lambdas
+list(~ mean(., trim = .2), ~ median(., na.rm = TRUE))
